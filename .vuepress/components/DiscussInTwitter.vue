081bb44a997ee76af10ce32844b918cd14d09162
Aleksey Razbakov <aleksey@razbakov.com>
2020-02-29 21:14:54 +0100

    update twitter rel

--- .vuepress/components/DiscussInTwitter.vue
+++ .vuepress/components/DiscussInTwitter.vue
@@ -2,7 +2,7 @@
   <div v-if="$page.frontmatter.tweet" class="container w-full md:max-w-xl mx-auto px-4 pb-8">
     <h2>Comments</h2>
     <blockquote>
-      You can comment on this post by replying to <a :href="$page.frontmatter.tweet" target="_blank" rel="nofollow">this tweet</a>.
+      You can comment on this post by replying to <a :href="$page.frontmatter.tweet" target="_blank" rel="noreferrer">this tweet</a>.
     </blockquote>
   </div>
 </template>
