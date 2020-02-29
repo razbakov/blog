efe427e5dd241641bdc937b240ffdf52c70040df
Aleksey Razbakov <aleksey@razbakov.com>
2020-02-29 16:43:57 +0100

    add twitter comments

--- .vuepress/components/DiscussInTwitter.vue
+++ .vuepress/components/DiscussInTwitter.vue
@@ -1,28 +1,8 @@
 <template>
   <div v-if="$page.frontmatter.tweet" class="container w-full md:max-w-xl mx-auto px-4 pb-8">
     <h2>Comments</h2>
-    <p>You can comment on this post by replying to this tweet.</p>
-    <div v-html="oembedTweet" />
+    <blockquote>
+      You can comment on this post by replying to <a :href="$page.frontmatter.tweet" target="_blank" rel="nofollow">this tweet</a>.
+    </blockquote>
   </div>
 </template>
-
-<script>
-import { Tweet } from 'vue-tweet-embed'
-
-export default {
-  components: { Tweet },
-  computed: {
-    async oembedTweet() {
-      if (!this.$page.frontmatter.tweet) {
-        return false
-      }
-
-      const tweetUrl = this.$page.frontmatter.tweet
-      const oembedResponse = await fetch(new Request(`https://publish.twitter.com/oembed?url=${tweetUrl}`))
-      const oembedTweet = oembedResponse.json()
-
-      return oembedTweet.html
-    }
-  }
-}
-</script>
