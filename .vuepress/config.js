fa0efcc2706218adfaae331887d43f4d8d34a57e
Aleksey Razbakov <aleksey@razbakov.com>
2020-03-02 11:55:29 +0100

    fix purgecss

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,7 +2,7 @@ const hostname = 'https://razbakov.com';
 
 const purgecss = require("@fullhuman/postcss-purgecss")({
   // Specify the paths to all of the template files in your project
-  content: ["./.vuepress/theme/**/*.*", "./!(node_modules)/**/*.md", "./*.md"],
+  content: ["./.vuepress/theme/**/*.*", "./.vuepress/components/**/*.*", "./!(node_modules)/**/*.md", "./*.md"],
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
