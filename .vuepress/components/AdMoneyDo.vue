77d588848ca82b6b5c0149e1bb181209b107b063
Aleksey Razbakov <aleksey@razbakov.com>
2020-03-09 22:10:45 +0100

    add moneydo ad

---
+++ .vuepress/components/AdMoneyDo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container bg-green-100 text-center p-4 w-full md:max-w-xl mx-auto border border-green-400">
+    <div class="mb-4">
+        Я запустил новый проект &mdash; Планер Лично Бюджета
+    </div>
+    <a class="bg-green-500 text-white hover:bg-green-100 hover:text-green-800 font-semibold py-2 px-4 border border-green-400 rounded shadow no-underline"
+        :href="`https://moneydo-budget-planner.netlify.com/?utm_medium=${from}`"
+        target="_blank"
+        rel="noopener noreferrer">
+      Открыть
+    </a>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: ['from']
+}
+</script>
