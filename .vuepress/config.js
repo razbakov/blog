adfb6b02ca436da89f20ba07f97272da453fffaa
Aleksey Razbakov <aleksey@razbakov.com>
2020-03-24 23:16:30 +0100

    add mafia

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,72 +1,81 @@
-const hostname = 'https://razbakov.com';
+const hostname = "https://razbakov.com";
 
 const purgecss = require("@fullhuman/postcss-purgecss")({
   // Specify the paths to all of the template files in your project
-  content: ["./.vuepress/theme/**/*.*", "./.vuepress/components/**/*.*", "./!(node_modules)/**/*.md", "./*.md"],
+  content: [
+    "./.vuepress/theme/**/*.*",
+    "./.vuepress/components/**/*.*",
+    "./!(node_modules)/**/*.md",
+    "./*.md"
+  ],
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 });
 
 module.exports = {
-  title: 'Aleksey Razbakov',
-  description: 'Web Developer',
+  title: "Aleksey Razbakov",
+  description: "Web Developer",
   themeConfig: {
     nav: [
-      { text: 'Blog', link: '/' },
+      { text: "Blog", link: "/" },
+      { text: "Mafia", link: "/mafia/" },
       // { text: 'Projects', link: '/projects/' },
-      { text: 'About', link: '/about/' },
+      { text: "About", link: "/about/" }
       // { text: 'Contact', link: '/contact/' },
     ],
-    hostname: hostname,
+    hostname: hostname
   },
   head: [
-    ['link', { rel: 'icon', href: '/icons/icon-512x512.png' }],
-    ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ['meta', { name: 'theme-color', content: '#2f80ed' }],
-    ['meta', { name: 'google-site-verification', content: 'lRlZ5U83s7NwROJk4j4gyNkI0MEvkmDMMxw1pD9pLdc' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: '/icons/icon-152x152.png' }],
-    ['link', { rel: 'mask-icon', href: '/icons/icon.svg', color: '#2f80ed' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/icons/icon-144x144.png' }],
-    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+    ["link", { rel: "icon", href: "/icons/icon-512x512.png" }],
+    ["link", { rel: "manifest", href: "/manifest.json" }],
+    ["meta", { name: "theme-color", content: "#2f80ed" }],
+    [
+      "meta",
+      { name: "google-site-verification", content: "lRlZ5U83s7NwROJk4j4gyNkI0MEvkmDMMxw1pD9pLdc" }
+    ],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    ["meta", { name: "apple-mobile-web-app-status-bar-style", content: "black" }],
+    ["link", { rel: "apple-touch-icon", href: "/icons/icon-152x152.png" }],
+    ["link", { rel: "mask-icon", href: "/icons/icon.svg", color: "#2f80ed" }],
+    ["meta", { name: "msapplication-TileImage", content: "/icons/icon-144x144.png" }],
+    ["meta", { name: "msapplication-TileColor", content: "#000000" }]
   ],
   plugins: {
-    'vuepress-plugin-reading-time': {},
-    '@vuepress/blog': {
+    "vuepress-plugin-reading-time": {},
+    "@vuepress/blog": {
       directories: [
         {
-          id: 'post',
-          dirname: 'blog',
-          path: '/',
+          id: "post",
+          dirname: "blog",
+          path: "/",
           pagination: {
-            perPagePosts: 2,
-          },
-        },
+            perPagePosts: 2
+          }
+        }
       ],
       frontmatters: [
         {
           id: "tag",
-          keys: ['tag', 'tags'],
-          path: '/tag/',
-          layout: 'Tag',
-          frontmatter: { title: 'Tag' },
-          itemlayout: 'Tag',
+          keys: ["tag", "tags"],
+          path: "/tag/",
+          layout: "Tag",
+          frontmatter: { title: "Tag" },
+          itemlayout: "Tag",
           pagination: {
             perPagePosts: 3
           }
-        },
+        }
       ],
       sitemap: {
         hostname: hostname
-      },
+      }
     },
-    'feed': {
-      canonical_base: hostname,
+    feed: {
+      canonical_base: hostname
     },
-    '@vuepress/pwa': {},
-    'seo': {}
+    "@vuepress/pwa": {},
+    seo: {}
   },
   postcss: {
     plugins: [
@@ -75,4 +84,4 @@ module.exports = {
       ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
     ]
   }
-}
+};
---
+++ mafia.md
@@ -0,0 +1,37 @@
+---
+permalink: /mafia
+language: en
+layout: page
+---
+
+# Mafia Party Game Online
+
+Mafia is a social deduction game.
+We gather online to play it via [Zoom](https://zoom.us/).
+
+<figure>
+  <img src="/img/mafia.png" alt="Mafia via Zoom" />
+  <figcaption>During the game.</figcaption>
+</figure>
+
+The game models a conflict between two groups:
+
+- an informed minority (the mafiosi)
+- and an uninformed majority (the citizens)
+
+At the start of the game, each player is secretly assigned a role affiliated with one of these teams.
+
+The game has two alternating phases:
+
+- first, a night phase, during which those with night killing powers may covertly kill other players,
+- and second, a day phase, in which surviving players debate the identities of players and vote to eliminate a suspect.
+
+The game continues until a faction achieves its win condition; for the city, this usually means eliminating the evil minority, while for the minority this usually means reaching numerical parity with the city and eliminating any rival evil groups.
+
+## How to join?
+
+Sign up and you will get emails about next games.
+
+<cta target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSeyon0uSGC9A0NJGnM7B_0HjWjdWfB9t34DPDx4zqI0gCB8pQ/viewform">Sign Up</cta>
+
+or drop me an email to [mafia@razbakov.com](mailto:mafia@razbakov.com)
